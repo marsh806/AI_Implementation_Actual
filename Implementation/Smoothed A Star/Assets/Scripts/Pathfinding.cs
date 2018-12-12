5d1ffffafa828f1961584db62a5879cf9f5c3264
Ethan Marshall <marsh806@umn.edu>
2018-12-11 18:17:46 -0600

    Uploading Unity Projects

---
+++ Implementation/Smoothed A Star/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System;
+
+public class Pathfinding : MonoBehaviour {
+	
+	PathRequestManager requestManager;
+	Grid grid;
+	
+	void Awake() {
+		requestManager = GetComponent<PathRequestManager>();
+		grid = GetComponent<Grid>();
+	}
+	
+	
+	public void StartFindPath(Vector3 startPos, Vector3 targetPos) {
+		StartCoroutine(FindPath(startPos,targetPos));
+	}
+	
+	IEnumerator FindPath(Vector3 startPos, Vector3 targetPos) {
+		
+		Stopwatch sw = new Stopwatch();
+		sw.Start();
+		
+		Vector3[] waypoints = new Vector3[0];
+		bool pathSuccess = false;
+		
+		Node startNode = grid.NodeFromWorldPoint(startPos);
+		Node targetNode = grid.NodeFromWorldPoint(targetPos);
+		startNode.parent = startNode;
+
+        int node_counter = 0;
+		
+		
+		if (startNode.walkable && targetNode.walkable) {
+			Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
+            print("grid max size: " + grid.MaxSize);
+			HashSet<Node> closedSet = new HashSet<Node>();
+			openSet.Add(startNode);
+			
+			while (openSet.Count > 0) {
+				Node currentNode = openSet.RemoveFirst();
+				closedSet.Add(currentNode);
+                node_counter = node_counter + 1;
+				
+				if (currentNode == targetNode) {
+					sw.Stop();
+					print ("Path found: " + sw.ElapsedMilliseconds + " ms");
+					pathSuccess = true;
+                    print("closed set size: " + closedSet.Count);
+                    print("Nodes created: " + node_counter);
+                    print("openset size: " + openSet.Count);
+                    break;
+				}
+				
+				foreach (Node neighbour in grid.GetNeighbours(currentNode)) {
+					if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+						continue;
+					}
+					
+					int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+					if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
+						neighbour.gCost = newMovementCostToNeighbour;
+						neighbour.hCost = GetDistance(neighbour, targetNode);
+						neighbour.parent = currentNode;
+						
+						if (!openSet.Contains(neighbour))
+							openSet.Add(neighbour);
+						else 
+							openSet.UpdateItem(neighbour);
+					}
+				}
+			}
+		}
+
+        pathSuccess = true;
+        yield return null;
+        Stopwatch sw2 = new Stopwatch();
+        sw2.Start();
+        if (pathSuccess) {
+			waypoints = RetracePath(startNode,targetNode);
+		}
+        requestManager.FinishedProcessingPath(waypoints,pathSuccess);
+        sw2.Stop();
+        print("Smoothing Complete: " + sw2.ElapsedMilliseconds + " ms");
+    }
+		
+	
+	Vector3[] RetracePath(Node startNode, Node endNode) {
+        //Stopwatch sw2 = new Stopwatch();
+        //sw2.Start();
+        List<Node> path = new List<Node>();
+		Node currentNode = endNode;
+		
+		while (currentNode != startNode) {
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		Vector3[] waypoints = SimplifyPath(path);
+		Array.Reverse(waypoints);
+        //sw2.Stop();
+        //print("Smoothing Complete: " + sw2.ElapsedMilliseconds + " ms");
+        return waypoints;
+		
+	}
+	
+	Vector3[] SimplifyPath(List<Node> path) {
+		List<Vector3> waypoints = new List<Vector3>();
+		Vector2 directionOld = Vector2.zero;
+		
+		for (int i = 1; i < path.Count; i ++) {
+			Vector2 directionNew = new Vector2(path[i-1].gridX - path[i].gridX,path[i-1].gridY - path[i].gridY);
+			if (directionNew != directionOld) {
+				waypoints.Add(path[i].worldPosition);
+			}
+			directionOld = directionNew;
+		}
+		return waypoints.ToArray();
+	}
+	
+	int GetDistance(Node nodeA, Node nodeB) {
+		int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+		int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+		
+		if (dstX > dstY)
+			return 14*dstY + 10* (dstX-dstY);
+		return 14*dstX + 10 * (dstY-dstX);
+	}
+	
+	
+}
